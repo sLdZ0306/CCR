978a56b56ef4124a187d649dc4deef8a9d9636cc
sLdZ0306 <88537886+sLdZ0306@users.noreply.github.com>
2023-08-02 17:22:56 +0800

    Delete modules directory

--- modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cross_gate import CrossGate
-from .dynamic_rnn import DynamicGRU
-from .mutihead_attention import MultiheadAttention
-from .tanh_attention import TanhAttention
-from .net_vlad import NetVLAD
